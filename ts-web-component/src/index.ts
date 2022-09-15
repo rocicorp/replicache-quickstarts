ed05ee953ed24e57a80c940037829ec6631aed31
Aaron Boodman <aaron@aaronboodman.com>
2022-09-14 23:16:23 -1000

    Remove non-react dirs

--- ts-web-component/src/index.ts
+++
@@ -1,6 +0,0 @@
-import { MyTodo } from "./component/my-todo";
-import { TodoItem } from "./component/todo-item";
-import "./style.css";
-
-window.customElements.define("my-todo", MyTodo);
-window.customElements.define("todo-item", TodoItem);
