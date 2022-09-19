62ef175dfaa39e843446174937b42c6e5c864b38
Cesar Alaestante <cesara@gmail.com>
2022-09-19 14:20:48 -0700

    fix: broken create space, streamline code more

--- ts-web-component/src/setup-replicache.ts
+++ ts-web-component/src/setup-replicache.ts
@@ -1,5 +1,3 @@
-import {getPokeReceiver} from './poke';
-import {createSpace, spaceExists} from './space';
 import {mutators} from '../../shared/mutators';
 import {Replicache} from 'replicache';
 import type {M} from '../../shared/mutators';
@@ -8,21 +6,11 @@ import {assert} from './assert';
 export async function setupReplicache(): Promise<Replicache<M>> {
   const {pathname} = window.location;
   const paths = pathname.split('/');
-  let listId =
-    paths.indexOf('listId') > -1 ? paths[paths.indexOf('listId') + 1] : '';
 
-  if (listId) {
-    const data = await spaceExists(listId);
-    if (data.spaceExists === false) {
-      const spaceRes = await createSpace(listId);
-      listId = spaceRes.spaceID;
-    }
-  }
-
-  if (!listId) {
-    const spaceRes = await createSpace();
-    listId = spaceRes.spaceID;
-    window.location.href = '/listId/' + listId;
+  const spaceID =
+    paths.indexOf('listID') > -1 ? paths[paths.indexOf('listID') + 1] : '';
+  if (!spaceID || !(await spaceExists(spaceID))) {
+    window.location.href = '/listID/' + (await createSpace());
   }
 
   // See https://doc.replicache.dev/licensing for how to get a license key.
@@ -31,16 +19,61 @@ export async function setupReplicache(): Promise<Replicache<M>> {
 
   const r = new Replicache<M>({
     licenseKey,
-    pushURL: `/api/replicache/push?spaceID=${listId}`,
-    pullURL: `/api/replicache/pull?spaceID=${listId}`,
-    name: listId,
+    pushURL: `/api/replicache/push?spaceID=${spaceID}`,
+    pullURL: `/api/replicache/pull?spaceID=${spaceID}`,
+    name: spaceID,
     mutators,
   });
 
-  const pokeReceiver = getPokeReceiver();
-  pokeReceiver(listId, async () => {
-    r.pull();
+  // Implements a Replicache poke using Server-Sent Events.
+  // If a "poke" message is received, it will pull from the server.
+  const ev = new EventSource(`/api/replicache/poke-sse?spaceID=${spaceID}`, {
+    withCredentials: true,
   });
+  ev.onmessage = async event => {
+    if (event.data === 'poke') {
+      await r.pull();
+    }
+  };
+
+  async function spaceExists(spaceID: string): Promise<boolean> {
+    const spaceExistRes = await fetchJSON('spaceExists', spaceID);
+    if (
+      spaceExistRes &&
+      typeof spaceExistRes === 'object' &&
+      typeof spaceExistRes.spaceExists === 'boolean'
+    ) {
+      return spaceExistRes.spaceExists;
+    }
+    throw new Error('Bad response from spaceExists');
+  }
+
+  async function createSpace(spaceID?: string): Promise<string> {
+    const createSpaceRes = await fetchJSON('createSpace', spaceID);
+    if (
+      createSpaceRes &&
+      typeof createSpaceRes === 'object' &&
+      typeof createSpaceRes.spaceID === 'string'
+    ) {
+      return createSpaceRes.spaceID;
+    }
+    throw new Error('Bad response from createSpace');
+  }
+
+  async function fetchJSON(apiName: string, spaceID: string | undefined) {
+    const res = await fetch(`/api/replicache/${apiName}`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body:
+        spaceID &&
+        JSON.stringify({
+          spaceID,
+        }),
+    });
+    return await res.json();
+  }
 
   return r;
 }
