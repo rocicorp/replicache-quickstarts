5a1262fce7782271ecaad041b6abe6d50471fd58
Cesar Alaestante <cesar@roci.dev>
2022-10-17 11:51:53 -0700

    feat: nextjs (#23)
    * feat: nextjs running as workspace working
    * version 0.1.9
    * update README

---
+++ client/nextjs/src/components/todo-list.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import {Todo, TodoUpdate} from 'replicache-quickstarts-shared';
+import {TodoItem} from './todo-item';
+
+const TodoList = ({
+  todos,
+  onUpdateTodo,
+  onDeleteTodo,
+}: {
+  todos: Todo[];
+  onUpdateTodo: (update: TodoUpdate) => void;
+  onDeleteTodo: (id: string) => void;
+}) => {
+  return (
+    <ul className="todo-list">
+      {todos.map(todo => (
+        <TodoItem
+          todo={todo}
+          key={todo.id}
+          onUpdate={update => onUpdateTodo(update)}
+          onDelete={() => onDeleteTodo(todo.id)}
+        />
+      ))}
+    </ul>
+  );
+};
+
+export default TodoList;
